e8cb54814dd1ec3bc820752f0a9827fb24ea8a24
Koen Deschacht <koendeschacht@gmail.com>
2018-11-13 22:56:29 +0100

    Formatting...

--- src/test/java/com/googlecode/jsonrpc4j/spring/AutoJsonRpcClientProxyCreator.java
+++ src/test/java/com/googlecode/jsonrpc4j/spring/AutoJsonRpcClientProxyCreator.java
@@ -29,96 +29,96 @@ import static org.springframework.util.ResourceUtils.CLASSPATH_URL_PREFIX;
  */
 @SuppressWarnings("unused")
 public class AutoJsonRpcClientProxyCreator implements BeanFactoryPostProcessor, ApplicationContextAware {
-	
-	private static final Logger logger = LoggerFactory.getLogger(AutoJsonRpcClientProxyCreator.class);
-	private ApplicationContext applicationContext;
-	private String scanPackage;
-	private URL baseUrl;
-	private ObjectMapper objectMapper;
-	private String contentType;
-	
-	@Override
-	public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) {
-		SimpleMetadataReaderFactory metadataReaderFactory = new SimpleMetadataReaderFactory(applicationContext);
-		DefaultListableBeanFactory defaultListableBeanFactory = (DefaultListableBeanFactory) beanFactory;
-		String resolvedPath = resolvePackageToScan();
-		logger.debug("Scanning '{}' for JSON-RPC service interfaces.", resolvedPath);
-		try {
-			for (Resource resource : applicationContext.getResources(resolvedPath)) {
-				if (resource.isReadable()) {
-					MetadataReader metadataReader = metadataReaderFactory.getMetadataReader(resource);
-					ClassMetadata classMetadata = metadataReader.getClassMetadata();
-					AnnotationMetadata annotationMetadata = metadataReader.getAnnotationMetadata();
-					String jsonRpcPathAnnotation = JsonRpcService.class.getName();
-					if (annotationMetadata.isAnnotated(jsonRpcPathAnnotation)) {
-						String className = classMetadata.getClassName();
-						String path = (String) annotationMetadata.getAnnotationAttributes(jsonRpcPathAnnotation).get("value");
-						logger.debug("Found JSON-RPC service to proxy [{}] on path '{}'.", className, path);
-						registerJsonProxyBean(defaultListableBeanFactory, className, path);
-					}
-				}
-			}
-		} catch (IOException e) {
-			throw new IllegalStateException(format("Cannot scan package '%s' for classes.", resolvedPath), e);
-		}
-	}
-	
-	/**
-	 * Converts the scanPackage to something that the resource loader can handleRequest.
-	 */
-	private String resolvePackageToScan() {
-		return CLASSPATH_URL_PREFIX + convertClassNameToResourcePath(scanPackage) + "/**/*.class";
-	}
-	
-	/**
-	 * Registers a new proxy bean with the bean factory.
-	 */
-	private void registerJsonProxyBean(DefaultListableBeanFactory defaultListableBeanFactory, String className, String path) {
-		BeanDefinitionBuilder beanDefinitionBuilder = BeanDefinitionBuilder
-				.rootBeanDefinition(JsonProxyFactoryBean.class)
-				.addPropertyValue("serviceUrl", appendBasePath(path))
-				.addPropertyValue("serviceInterface", className);
-		
-		if (objectMapper != null) {
-			beanDefinitionBuilder.addPropertyValue("objectMapper", objectMapper);
-		}
-		
-		if (contentType != null) {
-			beanDefinitionBuilder.addPropertyValue("contentType", contentType);
-		}
-		
-		defaultListableBeanFactory.registerBeanDefinition(className + "-clientProxy", beanDefinitionBuilder.getBeanDefinition());
-	}
-	
-	/**
-	 * Appends the base path to the path found in the interface.
-	 */
-	private String appendBasePath(String path) {
-		try {
-			return new URL(baseUrl, path).toString();
-		} catch (MalformedURLException e) {
-			throw new IllegalArgumentException(format("Cannot combine URLs '%s' and '%s' to valid URL.", baseUrl, path), e);
-		}
-	}
-	
-	@Override
-	public void setApplicationContext(ApplicationContext applicationContext) {
-		this.applicationContext = applicationContext;
-	}
-	
-	public void setBaseUrl(URL baseUrl) {
-		this.baseUrl = baseUrl;
-	}
-	
-	public void setScanPackage(String scanPackage) {
-		this.scanPackage = scanPackage;
-	}
-	
-	public void setObjectMapper(ObjectMapper objectMapper) {
-		this.objectMapper = objectMapper;
-	}
-	
-	public void setContentType(String contextType) {
-		this.contentType = contextType;
-	}
+
+    private static final Logger logger = LoggerFactory.getLogger(AutoJsonRpcClientProxyCreator.class);
+    private ApplicationContext applicationContext;
+    private String scanPackage;
+    private URL baseUrl;
+    private ObjectMapper objectMapper;
+    private String contentType;
+
+    @Override
+    public void postProcessBeanFactory(ConfigurableListableBeanFactory beanFactory) {
+        SimpleMetadataReaderFactory metadataReaderFactory = new SimpleMetadataReaderFactory(applicationContext);
+        DefaultListableBeanFactory defaultListableBeanFactory = (DefaultListableBeanFactory) beanFactory;
+        String resolvedPath = resolvePackageToScan();
+        logger.debug("Scanning '{}' for JSON-RPC service interfaces.", resolvedPath);
+        try {
+            for (Resource resource : applicationContext.getResources(resolvedPath)) {
+                if (resource.isReadable()) {
+                    MetadataReader metadataReader = metadataReaderFactory.getMetadataReader(resource);
+                    ClassMetadata classMetadata = metadataReader.getClassMetadata();
+                    AnnotationMetadata annotationMetadata = metadataReader.getAnnotationMetadata();
+                    String jsonRpcPathAnnotation = JsonRpcService.class.getName();
+                    if (annotationMetadata.isAnnotated(jsonRpcPathAnnotation)) {
+                        String className = classMetadata.getClassName();
+                        String path = (String) annotationMetadata.getAnnotationAttributes(jsonRpcPathAnnotation).get("value");
+                        logger.debug("Found JSON-RPC service to proxy [{}] on path '{}'.", className, path);
+                        registerJsonProxyBean(defaultListableBeanFactory, className, path);
+                    }
+                }
+            }
+        } catch (IOException e) {
+            throw new IllegalStateException(format("Cannot scan package '%s' for classes.", resolvedPath), e);
+        }
+    }
+
+    /**
+     * Converts the scanPackage to something that the resource loader can handleRequest.
+     */
+    private String resolvePackageToScan() {
+        return CLASSPATH_URL_PREFIX + convertClassNameToResourcePath(scanPackage) + "/**/*.class";
+    }
+
+    /**
+     * Registers a new proxy bean with the bean factory.
+     */
+    private void registerJsonProxyBean(DefaultListableBeanFactory defaultListableBeanFactory, String className, String path) {
+        BeanDefinitionBuilder beanDefinitionBuilder = BeanDefinitionBuilder
+                .rootBeanDefinition(JsonProxyFactoryBean.class)
+                .addPropertyValue("serviceUrl", appendBasePath(path))
+                .addPropertyValue("serviceInterface", className);
+
+        if (objectMapper != null) {
+            beanDefinitionBuilder.addPropertyValue("objectMapper", objectMapper);
+        }
+
+        if (contentType != null) {
+            beanDefinitionBuilder.addPropertyValue("contentType", contentType);
+        }
+
+        defaultListableBeanFactory.registerBeanDefinition(className + "-clientProxy", beanDefinitionBuilder.getBeanDefinition());
+    }
+
+    /**
+     * Appends the base path to the path found in the interface.
+     */
+    private String appendBasePath(String path) {
+        try {
+            return new URL(baseUrl, path).toString();
+        } catch (MalformedURLException e) {
+            throw new IllegalArgumentException(format("Cannot combine URLs '%s' and '%s' to valid URL.", baseUrl, path), e);
+        }
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) {
+        this.applicationContext = applicationContext;
+    }
+
+    public void setBaseUrl(URL baseUrl) {
+        this.baseUrl = baseUrl;
+    }
+
+    public void setScanPackage(String scanPackage) {
+        this.scanPackage = scanPackage;
+    }
+
+    public void setObjectMapper(ObjectMapper objectMapper) {
+        this.objectMapper = objectMapper;
+    }
+
+    public void setContentType(String contextType) {
+        this.contentType = contextType;
+    }
 }
